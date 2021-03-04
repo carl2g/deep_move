de8f585b122d5d3253291ab9b31f008cc59689e3
carl2g <degentilecarl@gmail.com>
2021-03-04 23:08:17 +0100

    add comment to dataset functions

--- deep_move/pipline.py
+++ deep_move/pipline.py
@@ -6,10 +6,24 @@ from sklearn.neural_network import MLPClassifier
 
 
 def reshape_input(x: np.ndarray):
+	'''
+		Flatten the image
+
+		Parameters:
+			x (np.ndarray): 3D np.ndarray
+
+		Returns:
+			The flatten images 
+	'''
 	return np.reshape(x, (x.shape[0], 28 * 28))
 
 
 def get_pipline():
+	'''
+		Create the pipline for the data
+		Returns:
+			The a Sklearn pipline 
+	'''
 	return Pipeline([
 		('reshape', FunctionTransformer(reshape_input)),
 		('sampler', RandomUnderSampler(random_state=0)),
